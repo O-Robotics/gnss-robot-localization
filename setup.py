8aab131a3d3d6ed1924c31f98aa54c83d7a9e99f
Ziye Li <144114689+Liziye725@users.noreply.github.com>
2025-04-29 18:45:14 +0200

    update file structure

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 
 setup(
     name='gnss_ekf',
-    version='0.0.0',
+    version='0.0.1',
     packages=['gnss_ekf'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + 'gnss_ekf']),
-        ('share/gnss_ekf/launch', ['launch/gnss_ekf.launch.py']),
+        ('share/gnss_ekf/launch', ['gnss_ekf.launch.py']),
         ('share/gnss_ekf', ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
+    maintainer='Li',
+    maintainer_email='ziyli@o-robotics.com',
     description='GNSS localization with EKF',
     license='Apache License 2.0',
     tests_require=['pytest'],
